fa100d2154c7ed88a034ce2a2185609d31a02d14
Anna van Biemen <annavanbiemen@gmail.com>
2025-05-27 16:52:53 +0200

    Use pytest-mock to test __main__.py.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,5 @@
 import contextlib
+import sys
 from runpy import run_module
 
 import pytest
@@ -13,9 +14,18 @@ def test_help(capfd: pytest.CaptureFixture[str]) -> None:
     assert "usage:" in capfd.readouterr().out
 
 
-def test_module_import() -> None:
+def test_module_import(monkeypatch) -> None:
+    def do_not_call() -> None:
+        raise RuntimeError
+
+    monkeypatch.setattr(sys.modules['app.main'], "main", do_not_call)
     run_module("app.__main__")
 
 
-def test_module_run() -> None:
+def test_module_run(monkeypatch, mocker) -> None:
+    mock = mocker.Mock()
+
+    monkeypatch.setattr(sys.modules['app.main'], "main", mock)
     run_module("app.__main__", run_name="__main__")
+
+    assert mock.call_count == 1
