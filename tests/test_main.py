f2499a4755e5443a5378fda6a2e623b777f754d5
Anna van Biemen <annavanbiemen@gmail.com>
2025-05-27 18:07:25 +0200

    Add simple csvfilter smoke test using mocker.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,6 @@
 import contextlib
 import sys
+from io import StringIO
 from runpy import run_module
 
 import pytest
@@ -12,6 +13,16 @@ def test_help(capfd: pytest.CaptureFixture[str]) -> None:
         main(["--help"])
 
     assert "usage:" in capfd.readouterr().out
+
+
+def test_csvfilter(mocker) -> None:
+    output = StringIO()
+    mocker.patch("app.di.stdin", new=StringIO("Column\nValue1\nValue2\n"))
+    mocker.patch("app.di.stdout", new=output)
+
+    main(["Column"])
+
+    assert output.getvalue() == "Column\r\nValue1\r\nValue2\r\n"
 
 
 def test_module_import(monkeypatch) -> None:
