a4dad318a6017336cb4dc1c3e01149e07ad47e8a
Anna van Biemen <annavanbiemen@gmail.com>
2025-05-28 10:08:06 +0200

    Add main test coverage.

--- tests/test_main.py
+++ tests/test_main.py
@@ -29,14 +29,14 @@ def test_module_import(monkeypatch) -> None:
     def do_not_call() -> None:
         raise RuntimeError
 
-    monkeypatch.setattr(sys.modules['app.main'], "main", do_not_call)
+    monkeypatch.setattr(sys.modules["app.main"], "main", do_not_call)
     run_module("app.__main__")
 
 
 def test_module_run(monkeypatch, mocker) -> None:
     mock = mocker.Mock()
 
-    monkeypatch.setattr(sys.modules['app.main'], "main", mock)
+    monkeypatch.setattr(sys.modules["app.main"], "main", mock)
     run_module("app.__main__", run_name="__main__")
 
     assert mock.call_count == 1
